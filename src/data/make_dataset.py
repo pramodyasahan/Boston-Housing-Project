066e9eb73d15e458a471b3d21f1d4d9c8c065e88
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-02-12 16:49:02 +0530

    feat: add load data function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,14 +2,13 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 
 
-def preprocess_data(input_path, output_path):
-    df = pd.read_csv(input_path)
-    print(df.isnull().sum())
+def preprocess_data(output_path, df):
     imputer = SimpleImputer(strategy='mean')
     df[['CRIM', 'ZN', 'INDUS', 'CHAS', 'AGE', 'LSTAT']] = imputer.fit_transform(
         df[['CRIM', 'ZN', 'INDUS', 'CHAS', 'AGE', 'LSTAT']])
     df.to_csv(output_path, index=False)
 
 
-if __name__ == '__main__':
-    preprocess_data('data/raw/HousingData.csv', 'data/processed/Clean_HousingData.csv')
+def load_data(data_path):
+    df = pd.read_csv(data_path)
+    return df
